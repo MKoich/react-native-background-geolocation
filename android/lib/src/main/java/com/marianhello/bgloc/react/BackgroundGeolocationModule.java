9c2ce843e359b15ce1e1d3b374708e70c33c67df
Mario Koch <mario@glazy.io>
2019-11-28 16:02:03 +0100

    …

--- android/lib/src/main/java/com/marianhello/bgloc/react/BackgroundGeolocationModule.java
+++ android/lib/src/main/java/com/marianhello/bgloc/react/BackgroundGeolocationModule.java
@@ -19,6 +19,7 @@ import com.marianhello.bgloc.PluginException;
 import com.marianhello.bgloc.data.BackgroundActivity;
 import com.marianhello.bgloc.data.BackgroundLocation;
 import com.marianhello.bgloc.react.data.LocationMapper;
+import com.marianhello.bgloc.react.headless.HeadlessTaskRunner;
 import com.marianhello.logging.LogEntry;
 import com.marianhello.logging.LoggerManager;
 
@@ -167,18 +168,6 @@ public class BackgroundGeolocationModule extends ReactContextBaseJavaModule impl
         });
     }
 
-    @Deprecated // use checkStatus as replacement
-    @ReactMethod
-    public void isLocationEnabled(Callback success, Callback error) {
-        logger.debug("Location services enabled check");
-        try {
-            success.invoke(facade.locationServicesEnabled());
-        } catch (PluginException e) {
-            logger.error("Location service checked failed: {}", e.getMessage());
-            error.invoke(ErrorMap.from(e));
-        }
-    }
-
     @ReactMethod
     public void showLocationSettings() {
         BackgroundGeolocationFacade.showLocationSettings(getContext());
@@ -321,9 +310,9 @@ public class BackgroundGeolocationModule extends ReactContextBaseJavaModule impl
     }
 
     @ReactMethod
-    public void headlessTask(String jsFunction, Callback success, Callback error) {
+    public void registerHeadlessTask(Callback success, Callback error) {
         logger.debug("Registering headless task");
-        facade.registerHeadlessTask(jsFunction);
+        facade.registerHeadlessTask(HeadlessTaskRunner.class.getName());
         success.invoke();
     }
 
